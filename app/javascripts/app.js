919b3e5784e691911494a306cf0fb94f1d2ae895
Wave <wave@WavedeMacBook-Pro.local>
2018-08-19 22:45:24 +0900

    modified:   app/index.html 	modified:   app/javascripts/app.js 	modified:   build/bundle.js 	deleted:    build/contracts/lovePass.json 	modified:   build/index.html 	modified:   contracts/Migrations.sol 	modified:   readme 	build/Migrations.json 	build/contracts/Migrations.json

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -1,4 +1,64 @@
-import "../stylesheets/app.css";
+// import "../stylesheets/app.css";
 import {  default as Web3 } from 'web3';
 import {  default as contract } from 'truffle-contract';
-import lovePass_artifacts from '../../build/contracts/lovePass.json'
+import Migrations_artifacts from '../../build/contracts/Migrations.json'
+
+var accounts, sim;
+var Migrations = contract(Migrations_artifacts);
+
+window.addEventListener('load', function() {
+    if (typeof web3 !== 'undefined') {
+        window.web3 = new Web3(web3.currentProvider);
+    } else {
+        window.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+    }
+
+    Migrations.setProvider(web3.currentProvider);
+    App.start();
+
+    $("#showNgoList").click(function() {
+        App.reNgoList();
+    });
+
+});
+
+window.App = {
+    start: function() {
+        var self = this;
+        web3.eth.getAccounts(function(err, accs) {
+            if (err != null) {
+                console.log(err);
+                return;
+            }
+            if (accs.length == 0) {
+              return;
+            }
+            accounts = accs;
+            console.log(accounts);
+            self.initializeLovePass();
+        });
+    },
+
+    initializeLovePass: function() {
+        var self = this;
+        Migrations.deployed().then(function(instance) {
+            sim = instance;
+            $("#confAddress").html(sim.address);
+        }).catch(function(e) {
+            console.log(e);
+        });
+    },
+
+    reNgoList: function(){
+        Migrations.deployed().then(function(instance) {
+            sim = instance;
+            console.log(sim);
+            sim.reNgoList().then(
+            function() {
+                console.log(sim.reNgoList.call());
+                return sim.reNgoList.call();
+            });
+        });
+    }
+
+};//loop for main
